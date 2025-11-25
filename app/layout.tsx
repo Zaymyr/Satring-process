8602f425ee2d0fe7af6b7233c5afb3adc0e9d1f0
Zaymyr <faustinbertrand1990@gmail.com>
2025-11-25 13:06:15 +0100

    Enable scrolling in main content area

--- app/layout.tsx
+++ app/layout.tsx
@@ -109,7 +109,7 @@ export default async function RootLayout({
                     </div>
                   </div>
                 </header>
-                <main className="flex-1 overflow-hidden">{children}</main>
+                <main className="flex-1 overflow-y-auto">{children}</main>
               </div>
             </QueryProvider>
           </I18nProvider>
